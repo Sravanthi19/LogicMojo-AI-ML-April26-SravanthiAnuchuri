d2abea4737af8b5855e532ac2682917cc765a5d2
sravanthi19 <sravanthianuchuri191@gmail.com9>
2026-05-08 11:56:30 +0530

    practice programms

---
+++ Assignment.py
@@ -0,0 +1,30 @@
+n = int(input('enter required number: '))
+
+# Upper half of the diamond (pyramid)
+for i in range(1,n+1):
+   for j in range(n-i):
+     print(' ', end = '')
+   for k in range(2*i-1):
+     print('*',end = '')
+   print()
+
+
+for i in range(n-1, 0, -1):
+    for j in range(n-i):
+        print(' ', end = '')
+    for k in range(2*i-1):
+        print('*', end = '')
+    print()
+
+  ################################### pyramid#########################3
+
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n + 1):
+    # Print leading spaces
+    for j in range(n - i):
+        print(" ", end=" ")
+    # Print stars
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
