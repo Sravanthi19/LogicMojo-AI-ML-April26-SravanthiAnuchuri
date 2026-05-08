4f0be91f84f9ea20d64cc9c3a42faa0ce995e58d
sravanthi19 <sravanthianuchuri191@gmail.com9>
2026-05-08 19:42:51 +0530

    practice programms

--- lists.py
+++ lists.py
@@ -6,3 +6,29 @@ for i in range(len(mylist)//2):
     break
 else:
   print('palindrome')
+
+#Write program count the blank spaces.
+
+mylist = ['i am sravanthi ']
+for i in mylist:
+  count = 0
+  for j in i:
+    if j == ' ':
+      count +=1
+  print(count)
+
+  ################ reverse string################
+s = 'i am sravanti'
+
+re = s[::-1]
+print(re)
+
+##############count no. of vowels and print all the vowels.################
+s = 'i am sravanthi'
+count = 0
+vowels = 'aeiou'
+for i in s:
+  if i in vowels:
+    count +=1
+    print(i)
+print(count)
